8a334f76c6abb8f14b1421c7506e180607c57b9d
Yasser Gribi <yasser.gribii@gmail.com>
2023-05-28 18:19:49 +0100

    Add tests for collections and products endpoints

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,7 +1,6 @@
-from http import client
-from django.contrib.auth.models import User
+from store.models import Collection
 from rest_framework import status
-from rest_framework.test import APIClient
+from model_bakery import baker
 import pytest
 
 @pytest.fixture
@@ -11,22 +10,30 @@ def create_collection(api_client):
     return do_create_collection
 
 @pytest.fixture
-def authenticate(api_client):
-    def do_authenticate(is_staff=False):
-        return api_client.force_authenticate(user=User(is_staff= is_staff))
-    return do_authenticate
+def delete_collection(api_client):
+    def do_delete_collection(collection):
+        return api_client.delete(f'/store/collections/{collection.id}/')
+    return do_delete_collection
 
+@pytest.fixture
+def patch_collection(api_client):
+    def do_patch_collection(collection, value):
+        return api_client.patch(f'/store/collections/{collection.id}/', value) 
+    return do_patch_collection
+
+
+# @pytest.mark.django_db
+# class TestCreateCollections:
+#     def test_if_user_is_anonymous_returns_401(self):
+#         # Arrange
+#             #empty
+#         #Act
+#         client = APIClient()
+#         response = client.post('/store/collections/', {'title' : 'a'})
+#         # Assert
+#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 @pytest.mark.django_db
 class TestCreateCollections:
-    # def test_if_user_is_anonymous_returns_401(self):
-    #     # Arrange
-    #         #empty
-    #     #Act
-    #     client = APIClient()
-    #     response = client.post('/store/collections/', {'title' : 'a'})
-    #     # Assert
-    #     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
     def test_if_user_is_anonymous_returns_401(self, create_collection):
         response = create_collection({'title' : 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
@@ -53,3 +60,95 @@ class TestCreateCollections:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        #arrange
+        collection = baker.make(Collection)
+        #act
+        response = api_client.get(f'/store/collections/{collection.id}/')
+        #assert
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == collection.id
+        assert response.data['title'] == collection.title
+    
+    def test_if_collection_exists_returns_200(self, api_client):
+
+        response = api_client.get(f'/store/collections/{1}/')
+        #assert
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestDeleteCollection:
+    def test_if_user_is_anonymous_returns_401(self, delete_collection):
+        #arrange
+        collection = baker.make(Collection)
+        #assert
+        response = delete_collection(collection)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_if_user_is_not_admin_returns_403(self, authenticate, delete_collection):
+        authenticate()
+
+        collection = baker.make(Collection)
+
+        response = delete_collection(collection)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_collection_is_not_found_returns_404(self, authenticate, delete_collection):
+        authenticate(is_staff = True ) 
+        
+        response = delete_collection(Collection)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_user_is_admin_and_deleted_returns_204(self, authenticate, delete_collection):
+        authenticate(is_staff = True ) 
+        #arrange
+        collection = baker.make(Collection)
+        #assert
+        response = delete_collection(collection)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        response = delete_collection(collection)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        
+@pytest.mark.django_db       
+class TestUpdateCollection:
+    def test_if_user_is_anonymous_returns_401(self, patch_collection):
+        collection = baker.make(Collection)
+
+        response = patch_collection(collection, {'title' : 'a'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self,authenticate,patch_collection):
+        authenticate()
+        collection = baker.make(Collection)
+
+        response = patch_collection(collection, {'title' : 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_invalid_returns_400(self,authenticate, patch_collection):
+        authenticate(is_staff = True)
+        collection = baker.make(Collection)
+
+        response = patch_collection(collection, {'title' : ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_user_is_admin_returns_200(self,authenticate, patch_collection):
+        authenticate(is_staff = True)
+        collection = baker.make(Collection)
+
+        response = patch_collection(collection, {'title' : 'a'})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == collection.id
+        assert response.data['title'] == 'a'
+        
+        
+
---
+++ store/tests/test_products.py
@@ -0,0 +1,178 @@
+from store.models import Product , Collection
+from rest_framework import status
+from model_bakery import baker
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.fixture
+def create_product(api_client):
+    def do_create_product(product):
+        return api_client.post('/store/products/', product)
+    return do_create_product
+
+@pytest.fixture
+def delete_product(api_client):
+    def do_delete_product(product):
+        return api_client.delete(f'/store/products/{product.id}/')
+    return do_delete_product
+
+@pytest.fixture
+def patch_product(api_client):
+    def do_patch_product(product, values):
+        return api_client.patch(f'/store/products/{product.id}/', values) 
+    return do_patch_product
+
+@pytest.mark.django_db
+class TestCreationProduct:
+    def test_if_user_is_anonymous_returns_401(self, create_product):
+
+        collection = baker.make(Collection)
+        response = create_product({"title": "a",
+                                    "description": "a",
+                                    "slug": "-",
+                                    "inventory": 4,
+                                    "unit_price": 1,  
+                                    "collection": collection.id,
+                                    "images": []
+                                })
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_product):
+        authenticate()
+
+        collection = baker.make(Collection)
+        response = create_product({"title": "a",
+                                    "description": "a",
+                                    "slug": "-",
+                                    "inventory": 4,
+                                    "unit_price": 1,  
+                                    "collection": collection.id,
+                                    "images": []
+                                })
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_product):
+        authenticate(is_staff = True ) 
+
+        collection = baker.make(Collection)
+        response = create_product({"title": "",
+                                    "description": "a",
+                                    "slug": "-",
+                                    "inventory": 4,
+                                    "unit_price": 0,  
+                                    "collection": collection.id,
+                                    "images": []
+                                })
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+        
+    def test_if_data_is_valid_returns_201(self , authenticate, create_product):
+        authenticate(is_staff = True)
+
+        collection = baker.make(Collection)
+        response = create_product({"title": "a",
+                                    "description": "a",
+                                    "slug": "-",
+                                    "inventory": 4,
+                                    "unit_price": 1,  
+                                    "collection": collection.id,
+                                    "images": []
+                                })
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveProduct:
+    def test_if_product_exists_returns_200(self, api_client):
+        #arrange
+        product = baker.make(Product)
+        #act
+        response = api_client.get(f'/store/products/{product.id}/')
+        #assert
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == product.id
+        assert response.data['title'] == product.title
+        assert response.data['unit_price'] == product.unit_price
+        assert response.data['collection'] == product.collection.id
+
+    def test_if_product_does_not_exist_returns_404(self, api_client):
+
+        response = api_client.get(f'/store/products/{-1}/')
+        #assert
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+@pytest.mark.django_db
+class TestDeleteProduct:
+    def test_if_user_is_anonymous_returns_401(self,delete_product):
+        product = baker.make(Product)
+
+        response = delete_product(product)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_if_user_is_not_admin_returns_403(self,authenticate ,delete_product):
+        authenticate()
+        product = baker.make(Product)
+
+        response = delete_product(product)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_product_is_not_found_returns_404(self, authenticate, delete_product):
+        authenticate(is_staff = True ) 
+
+        response = delete_product(Product)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_product_is_deleted_returns_204(self,authenticate ,delete_product):
+        authenticate(is_staff = True)
+        product = baker.make(Product)
+
+        response = delete_product(product)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert Product.objects.all().count() == 0
+
+        
+@pytest.mark.django_db
+class TestUpdateProduct: 
+    def test_if_user_is_anonymous_returns_401(self, patch_product):
+        product = baker.make(Product)
+    
+        response = patch_product(product, {'title' : 'b', 'unit_price' : 11 })
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_if_user_is_not_admin_returns_403(self, authenticate, patch_product):
+        authenticate()
+        product = baker.make(Product)
+    
+        response = patch_product(product, {'title' : 'b', 'unit_price' : 11 })
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_invalid_returns_400(self, authenticate, patch_product):
+        authenticate(is_staff = True)
+        product = baker.make(Product)
+    
+        response = patch_product(product, {'title' : '', 'unit_price' : 0 })
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_200(self, authenticate, patch_product):
+        authenticate(is_staff = True)
+        product = baker.make(Product)
+    
+        response = patch_product(product, {'title' : 'a', 'unit_price' : 13 })
+
+        assert response.status_code == status.HTTP_200_OK
+        #product.refresh_from_db()
+        assert response.data['id'] == product.id
+        assert response.data['title'] == 'a'
+        assert response.data['unit_price'] == 13
